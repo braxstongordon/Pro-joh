8b5febc2c58fe668127fefe47c0e009794765e41
braxstongordon <184780193+braxstongordon@users.noreply.github.com>
2025-01-20 16:12:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function sum (a,b) {
+    return a + b
+}
+sum (3,1)
+function greet(){
+    return ("hello gordon")
+}
+greet()
+function user(name) {
+    alert('hello & (name)');
+}
+user('gordon')
+function greet (name ="Guest", time ='day') {
+    return 'good $(time), $(name)!';
+}
+greetings()
